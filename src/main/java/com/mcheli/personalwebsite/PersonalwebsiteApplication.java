1d3fd9d248d56e9c2bd2ed037699fdf07fb45618
Mark Cheli <mpcheli7@gmail.com>
2017-08-05 21:56:07 -0400

    Java Backend

---
+++ src/main/java/com/mcheli/personalwebsite/PersonalwebsiteApplication.java
@@ -0,0 +1,15 @@
+package com.mcheli.personalwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class PersonalwebsiteApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PersonalwebsiteApplication.class, args);
+
+	}
+}
