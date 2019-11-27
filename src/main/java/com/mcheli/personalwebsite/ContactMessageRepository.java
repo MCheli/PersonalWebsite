9c0ab3dccab9dc3a23c67a4056d285e605dcec16
Mark Cheli <mpcheli7@gmail.com>
2019-11-27 15:59:11 -0500

    Removed Java component

--- src/main/java/com/mcheli/personalwebsite/ContactMessageRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mcheli.personalwebsite;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ContactMessageRepository extends CrudRepository<ContactMessage, Long>{
-}
